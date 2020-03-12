bdc29045a4d93048a295e465b5cc783f618e00a0
Sihyun Kim <legitcode267@gmail.com>
2020-03-12 08:47:33 -0600

    Updates

---
+++ precache-manifest.acdead9650b4d3d29419fcb632c19da3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7019a578cefc2b9c2612b572702652",
+    "url": "/news-viewer/index.html"
+  },
+  {
+    "revision": "88c1b034de709f1dd427",
+    "url": "/news-viewer/static/css/main.77e97215.chunk.css"
+  },
+  {
+    "revision": "ddef86e19300480cc8c0",
+    "url": "/news-viewer/static/js/2.ddd6e429.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/news-viewer/static/js/2.ddd6e429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88c1b034de709f1dd427",
+    "url": "/news-viewer/static/js/main.024ba546.chunk.js"
+  },
+  {
+    "revision": "23732484adad2887e665",
+    "url": "/news-viewer/static/js/runtime-main.54240f8d.js"
+  }
+]);
