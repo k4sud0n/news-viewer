be12c146dd55deb74850cd53f73d90e1da174056
Sihyun Kim <legitcode267@gmail.com>
2020-03-12 08:44:33 -0600

    Finish

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styled, { css } from 'styled-components';
+import { NavLink } from 'react-router-dom';
 import styles from './Categories.scss';
 
 const cx = classNames.bind(styles);
 
-const Category = styled.div`
+const Category = styled(NavLink)`
   font-size: 1.125rem;
   cursor: pointer;
   white-space: pre;
@@ -17,16 +18,14 @@ const Category = styled.div`
     color: #495057;
   }
 
-  ${props =>
-    props.active &&
-    css`
-      font-weight: 600;
-      border-bottom: 2px solid #22b8cf;
-      color: #22b8cf;
-      &:hover {
-        color: #3bc9db;
-      }
-    `}
+  &.active {
+    font-weight: 600;
+    border-bottom: 2px solid #22b8cf;
+    color: #22b8cf;
+    &:hover {
+      color: #3bc9db;
+    }
+  }
 
   & + & {
     margin-left: 1rem;
@@ -70,8 +69,9 @@ const Categories = ({ onSelect, category }) => {
       {categories.map(c => (
         <Category
           key={c.name}
-          active={category === c.name}
-          onClick={() => onSelect(c.name)}
+          activeClassName="active"
+          exact={c.name === 'all'}
+          to={c.name === 'all' ? '/' : `/${c.name}`}
         >
           {c.text}
         </Category>
--- src/pages/NewsPage.js
+++ src/pages/NewsPage.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Categories from '../components/Categories';
-import NewsList from '../components/';
+import NewsList from '../components/NewsList';
 
 const NewsPage = ({ match }) => {
+  // 카테고리가 선택되지 않았으면 기본값 all로 사용
   const category = match.params.category || 'all';
 
   return (
